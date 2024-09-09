696c95f4afda50fde7c4fce2a8e4c7fa6c70e379
hamzathul <83232962+hamzathul@users.noreply.github.com>
2024-09-09 17:10:15 +0530

    Adding new changes

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import { getFirestore, collection, getDocs } from "firebase/firestore";
 
 // For Firebase JS SDK v9.0.0 and later
 const firebaseConfig = {
-    // apiKey: "",  Paste your API Key here
-    authDomain: "fir-d80ce.firebaseapp.com",
-    projectId: "fir-d80ce",
-    storageBucket: "fir-d80ce.appspot.com",
-    messagingSenderId: "1072731959063",
-    appId: "1:1072731959063:web:f83931f4695d30636bd152",
-    measurementId: "G-NY2WK0J0B2"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
